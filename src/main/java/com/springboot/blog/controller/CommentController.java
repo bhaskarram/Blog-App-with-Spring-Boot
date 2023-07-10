dff2e7148fbf60cf32d35ae2fbf63a84671fe92d
bhaskarram <bhaskar1208200r@gmail.com>
2023-07-10 06:53:33 +0530

    Implemented Comments layer

---
+++ src/main/java/com/springboot/blog/controller/CommentController.java
@@ -0,0 +1,56 @@
+package com.springboot.blog.controller;
+
+import com.springboot.blog.payload.CommentDto;
+import com.springboot.blog.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class CommentController {
+
+    private CommentService commentService;
+
+    @Autowired
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @PostMapping("/posts/{postId}/comments")
+    public ResponseEntity<CommentDto> createComment(@PathVariable(name = "postId") long postId,
+                                                    @RequestBody CommentDto commentDto){
+        return new ResponseEntity<>(commentService.createComment(postId,commentDto), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/posts/{postId}/comments")
+    public List<CommentDto> getCommentsByPostID(@PathVariable(name = "postId") Long postId){
+
+        return commentService.getCommentsByPostId(postId);
+    }
+
+    @GetMapping("/posts/{postId}/comments/{commentId}")
+    public ResponseEntity<CommentDto> getCommentsById(
+            @PathVariable(name = "postId") long postId,
+            @PathVariable(name ="commentId") long commentId){
+        return new ResponseEntity<>(commentService.getCommentById(postId,commentId), HttpStatus.OK);
+    }
+
+
+    @PutMapping("/posts/{postId}/comments/{commentsId}")
+    public ResponseEntity<CommentDto> updateComment(@PathVariable(name = "postId") long postId,
+                                                    @PathVariable(name = "commentId") long commentId,
+                                                    @RequestBody CommentDto commentDto){
+        return  new ResponseEntity<>(commentService.updateComment(postId,commentId,commentDto), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/posts/{posts}/comments/{commentsId}")
+    public ResponseEntity<String> deleteComment(@PathVariable(value = "postId") long postId,
+                                                @PathVariable(value = "commentId") long commentId){
+        commentService.deleteComment(postId,commentId);
+        return new ResponseEntity<>("Comment deleted successfully", HttpStatus.OK);
+    }
+}
--- src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
+++ src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
@@ -1,12 +1,20 @@
 package com.springboot.blog.service.impl;
 
-import com.springboot.blog.dto.PostDto;
+import com.springboot.blog.payload.PostDto;
 import com.springboot.blog.entity.Post;
+import com.springboot.blog.exception.ResourceNotFoundException;
+import com.springboot.blog.payload.PostResponse;
 import com.springboot.blog.repository.PostRepository;
 import com.springboot.blog.service.PostService;
-import jakarta.websocket.server.ServerEndpoint;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PostServiceImpl implements PostService {
@@ -20,22 +28,78 @@ public class PostServiceImpl implements PostService {
 
     @Override
     public PostDto createPost(PostDto postDto) {
+        //Dto into Entity
+        Post post = mapToEntity(postDto);
+        Post newPost = postRepository.save(post);
 
-       //Convert DTO into entity
+        //Entity to DTO
+        PostDto postRespone = maptoDto(newPost);
+        return postRespone;
+    }
+
+    @Override
+    public PostResponse getAllPosts(int pageNo, int pageSize, String sortBy, String sortDir) {
+
+        Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortDir).ascending()
+                   : Sort.by(sortBy).descending();
+
+        Pageable pageable = PageRequest.of(pageNo,pageSize, sort);
+        Page<Post> posts = postRepository.findAll(pageable);
+        List<Post> listOfPosts = posts.getContent();
+        List<PostDto> content =  listOfPosts.stream().map(post ->maptoDto(post)).collect(Collectors.toList());
+
+        PostResponse postResponse = new PostResponse();
+        postResponse.setContent(content);
+        postResponse.setPageNo(posts.getNumber());
+        postResponse.setPageSize(posts.getSize());
+        postResponse.setTotalElements(posts.getTotalElements());
+        postResponse.setTotalPages(posts.getTotalPages());
+        postResponse.setLast(posts.isLast());
+
+        return postResponse;
+    }
+
+    @Override
+    public PostDto getPostById(long id) {
+        Post post = postRepository.findById(id).orElseThrow(() ->new ResourceNotFoundException("Post","id",id));
+        return maptoDto(post);
+    }
+
+    @Override
+    public PostDto updatePost(PostDto postDto, long id) {
+        Post post = postRepository.findById(id).orElseThrow(() ->new ResourceNotFoundException("Post","id",id));
+
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+
+        Post updatedPost = postRepository.save(post);
+        return maptoDto(updatedPost);
+    }
+
+    @Override
+    public void deletePost(long id) {
+        Post post = postRepository.findById(id).orElseThrow(() ->new ResourceNotFoundException("Post","id",id));
+        postRepository.delete(post);
+    }
+
+    //Convert DTO to entity
+    private Post mapToEntity(PostDto postDto){
         Post post = new Post();
         post.setTitle(postDto.getTitle());
         post.setDescription(postDto.getDescription());
         post.setContent(postDto.getContent());
 
-        Post newPost = postRepository.save(post);
+        return post;
+    }
+    //Convert entity to DTO
+    private PostDto maptoDto(Post post){
+        PostDto postDto = new PostDto();
+        postDto.setId(post.getId());
+        postDto.setTitle(post.getTitle());
+        postDto.setDescription(post.getDescription());
+        postDto.setContent(post.getContent());
 
-        //Convert entity to DTO
-        PostDto postRespone = new PostDto();
-        postRespone.setId(newPost.getId());
-        postRespone.setTitle(newPost.getTitle());
-        postRespone.setDescription(newPost.getDescription());
-        postRespone.setContent(newPost.getContent());
-
-        return postRespone;
+        return postDto;
     }
 }
